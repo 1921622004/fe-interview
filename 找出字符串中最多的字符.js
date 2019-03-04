19a302d8d63af8012eaf58e60bc420cf9c41d36e
1921622004 <1921622004qwer@gmail.com>
2019-03-04 18:01:54 +0800

    format

--- 找出字符串中最多的字符.js
+++ 找出字符串中最多的字符.js
@@ -1,20 +1,4 @@
-// 翻转字符串
-
-function reverseStr1(str) {
-    return str.split('').reverse().join('');
-}
-
-function reverseStr2(str2) {
-    var res = '';
-    for (var i = str2.length - 1; i > -1; i--) {
-        res += str2[i]
-    };
-    return res
-}
-
-
 // 找出字符串中最多的字符
-
 function findMax(str) {
     var obj = {};
     for (var i in str) {
